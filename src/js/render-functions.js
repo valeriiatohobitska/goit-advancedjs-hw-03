44625c4b7b7c127502433328dad8cc661bad7c7e
valeriia <valeryankin92@gmail.com>
2025-06-04 19:30:18 +0300

    update files

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,23 +1,23 @@
 export function renderGallery(images) {
   const gallery = document.querySelector('.gallery');
-
-  // Очищення перед новим запитом
   gallery.innerHTML = '';
 
   const markup = images
     .map(image => {
       return `
-      <a class="gallery__item" href="${image.largeImageURL}">
-        <div class="photo-card">
-          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          <div class="info">
-            <p><b>Likes:</b> ${image.likes}</p>
-            <p><b>Views:</b> ${image.views}</p>
-            <p><b>Comments:</b> ${image.comments}</p>
-            <p><b>Downloads:</b> ${image.downloads}</p>
+      <li class="gallery__item">
+        <a href="${image.largeImageURL}">
+          <div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+              <p><b>Likes:</b> ${image.likes}</p>
+              <p><b>Views:</b> ${image.views}</p>
+              <p><b>Comments:</b> ${image.comments}</p>
+              <p><b>Downloads:</b> ${image.downloads}</p>
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </li>
     `;
     })
     .join('');
